c3d1a5b1cd9fd0776493f5e59d3b90d2a136d2ab
Koz-Koz <kozjulia@hotmail.fr>
2018-07-31 16:34:14 +0200

    First commit heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+	root 'users#home'
   get '/user', to: 'users#new'
   post '/user', to: 'users#create'
 end
--- db/migrate/20180731130636_create_users.rb
+++ db/migrate/20180731130636_create_users.rb
@@ -4,7 +4,6 @@ class CreateUsers < ActiveRecord::Migration[5.2]
 		t.string :username
 		t.string :email
 		  t.text :bio
-      t.timestamps
     end
   end
 end
